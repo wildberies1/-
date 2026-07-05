b5b78b2016eb8eb2b5ef36b3c49f0d7f8ba87613
wildberies1 <GitHub_1@mail.ru>
2026-07-05 22:30:45 +0300

    Create  sw.js

---
+++ sw.js
@@ -0,0 +1,67 @@
+const CACHE_NAME = 'milordy-v1';
+const urlsToCache = [
+  './client.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Push notification handler
+self.addEventListener('push', event => {
+  const data = event.data ? event.data.json() : {};
+  
+  const options = {
+    body: data.body || 'Новое сообщение',
+    icon: '👑',
+    badge: '👑',
+    vibrate: [200, 100, 200],
+    tag: 'milordy-message',
+    requireInteraction: true,
+    actions: [
+      { action: 'open', title: 'Открыть' },
+      { action: 'close', title: 'Закрыть' }
+    ]
+  };
+  
+  event.waitUntil(
+    self.registration.showNotification('Милорды', options)
+  );
+});
+
+// Notification click handler
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  
+  if (event.action === 'open' || !event.action) {
+    event.waitUntil(
+      clients.openWindow('./client.html')
+    );
+  }
+});
